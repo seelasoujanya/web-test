dd92c45df4807db5ee64987cad24c1c334812e65
murarisabavath <murari.sabavath@theleanapps.com>
2024-04-02 12:45:07 +0530

    add sonar properties

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
     })
   );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -22,12 +29,13 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('deliver-upgrade-frontend');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'deliver-upgrade-frontend app is running!'
-    );
+  it(`should add two numbers correctly`, () => {
+    const result = component.addNumbers(3, 4);
+    expect(result).toBeFalsy();
+  });
+
+  it(`should add two numbers incorrectly`, () => {
+    const result = component.addNumbers(11, 4);
+    expect(result).toBeTruthy();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'deliver-upgrade-frontend';
+
+  addNumbers(a: number, b: number): boolean {
+    const sum = a + b;
+    if (sum > 10) {
+      return true;
+    }
+    return false;
+  }
 }
