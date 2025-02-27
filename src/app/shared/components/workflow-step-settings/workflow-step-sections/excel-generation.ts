cc596f638a46de0503c6b8e55e64c4dc51bade76
srijamangali <srija.mangali@theleanapps.com>
2025-02-27 20:27:30 +0530

    DR-465 Changes

--- src/app/shared/components/workflow-step-settings/workflow-step-sections/excel-generation.ts
+++ src/app/shared/components/workflow-step-settings/workflow-step-sections/excel-generation.ts
@@ -18,6 +18,7 @@ export const excelGeneration: IWorkflowConfigurationSection[] = [
           'RUSSIA_PPI_REGISTRATION',
           'MAHASZ_HUNGARY',
           'GRAMMO_GREECE',
+          'IFPI_ISRAEL',
         ],
         description: 'Select a Template',
         required: false,
