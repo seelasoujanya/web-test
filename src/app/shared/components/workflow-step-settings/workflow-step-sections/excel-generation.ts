379aabfb3f9a63a32213faa6a2e875fcebb5b15f
srijamangali <srija.mangali@theleanapps.com>
2025-02-27 21:17:18 +0530

    DR-468 Added REGISTRATIONS_CZECH Society

--- src/app/shared/components/workflow-step-settings/workflow-step-sections/excel-generation.ts
+++ src/app/shared/components/workflow-step-settings/workflow-step-sections/excel-generation.ts
@@ -18,6 +18,7 @@ export const excelGeneration: IWorkflowConfigurationSection[] = [
           'RUSSIA_PPI_REGISTRATION',
           'MAHASZ_HUNGARY',
           'GRAMMO_GREECE',
+          'REGISTRATIONS_CZECH',
         ],
         description: 'Select a Template',
         required: false,
