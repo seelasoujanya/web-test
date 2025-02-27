f9b4245d6c1288aa77d680f76ccd9a834ad7777c
srijamangali <srija.mangali@theleanapps.com>
2025-02-27 17:00:21 +0530

    DR-462 Added SOUNDEXCHANGE_NORTHAMERICA Society

--- src/app/shared/components/workflow-step-settings/workflow-step-sections/excel-generation.ts
+++ src/app/shared/components/workflow-step-settings/workflow-step-sections/excel-generation.ts
@@ -18,6 +18,7 @@ export const excelGeneration: IWorkflowConfigurationSection[] = [
           'RUSSIA_PPI_REGISTRATION',
           'MAHASZ_HUNGARY',
           'GRAMMO_GREECE',
+          'SOUNDEXCHANGE_NORTHAMERICA',
         ],
         description: 'Select a Template',
         required: false,
